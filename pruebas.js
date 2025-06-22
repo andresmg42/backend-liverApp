938fc6135ad9a6b6fec186899c34819bf976b95b
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-06-22 08:25:53 -0500

    predespliegue, se elimino pruebas.py y se ignoro .env

--- pruebas.js
+++
@@ -1,8 +0,0 @@
-import { preguntas_prueva } from "./preguntas_pruevas.js";
-import ProgressDAO from "./api/DAO/ProgressDAO.js";
-
-const quiz=new ProgressDAO();
-
-const {user_id,quiz_id,section_slug,answers}=preguntas_prueva;
-
-quiz.saveProgress(user_id,quiz_id,section_slug,answers)
